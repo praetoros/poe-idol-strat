c9192af3f6d321d0d6518fccc267151b503cbebf
Tom Alle <tom@tomalle.com>
2025-03-03 02:04:15 +1100

    fix mobile header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
 	return (
 		<header className="w-full bg-gray-800 px-6 py-4 text-white">
-			<div className="container mx-auto flex items-center justify-between">
+			<div className="container mx-auto flex flex-col items-center justify-between md:flex-row">
 				<a href="/">
 					<img src="/favico.webp" alt="Logo" className="h-8 w-auto" />
 				</a>
@@ -18,6 +18,15 @@ const Header = () => {
 						className="hover:underline"
 					>
 						Made by Praetoros
+					</a>
+					<span className="mx-2">|</span>
+					<a
+						href="https://github.com/praetoros/poe-idol-strat/issues"
+						target="_blank"
+						rel="noreferrer"
+						className="hover:underline"
+					>
+						Report issues on the GitHub
 					</a>
 				</div>
 			</div>
